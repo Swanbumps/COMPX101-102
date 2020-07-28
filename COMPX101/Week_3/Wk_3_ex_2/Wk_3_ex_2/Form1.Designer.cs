f706646826a8afccb0ff9125b3704bf1a0578109
Swanbumps <Swanbumps@users.noreply.github.com>
2020-07-28 16:58:05 +1200

    doing work

---
+++ COMPX101/Week_3/Wk_3_ex_2/Wk_3_ex_2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Wk_3_ex_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDrawTriangle = new System.Windows.Forms.Button();
+            this.buttonDrawSquare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonDrawTriangle
+            // 
+            this.buttonDrawTriangle.Location = new System.Drawing.Point(253, 156);
+            this.buttonDrawTriangle.Name = "buttonDrawTriangle";
+            this.buttonDrawTriangle.Size = new System.Drawing.Size(88, 23);
+            this.buttonDrawTriangle.TabIndex = 0;
+            this.buttonDrawTriangle.Text = "Draw Triangle";
+            this.buttonDrawTriangle.UseVisualStyleBackColor = true;
+            this.buttonDrawTriangle.Click += new System.EventHandler(this.buttonDrawTriangle_Click);
+            // 
+            // buttonDrawSquare
+            // 
+            this.buttonDrawSquare.Location = new System.Drawing.Point(374, 156);
+            this.buttonDrawSquare.Name = "buttonDrawSquare";
+            this.buttonDrawSquare.Size = new System.Drawing.Size(79, 23);
+            this.buttonDrawSquare.TabIndex = 1;
+            this.buttonDrawSquare.Text = "Draw Square";
+            this.buttonDrawSquare.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDrawSquare);
+            this.Controls.Add(this.buttonDrawTriangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDrawTriangle;
+        private System.Windows.Forms.Button buttonDrawSquare;
+    }
+}
+
---
+++ COMPX101/Week_3/Wk_3_ex_2/Wk_3_ex_2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wk_3_ex_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDrawTriangle_Click(object sender, EventArgs e)
+        {
+            const Point corner1, 10, 10;
+            const Point corner2, 10, 100;
+            const Point corner3, 100, 10;
+
+        }
+    }
+}
