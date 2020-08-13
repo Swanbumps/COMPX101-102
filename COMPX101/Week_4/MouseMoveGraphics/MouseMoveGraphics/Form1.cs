d4074e0859881f9ecf9df54958e42b1f61f5f500
Swanbumps <41099838+Swanbumps@users.noreply.github.com>
2020-08-14 01:00:34 +1200

    ex1 complete

--- COMPX101/Week_4/MouseMoveGraphics/MouseMoveGraphics/Form1.cs
+++ COMPX101/Week_4/MouseMoveGraphics/MouseMoveGraphics/Form1.cs
@@ -12,6 +12,7 @@ namespace MouseMoveGraphics
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +20,16 @@ namespace MouseMoveGraphics
 
         private void pictureBoxDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            Graphics paper = pictureBoxDisplay.CreateGraphics();
-            Pen pen1 = new Pen(colorDialog1.Color, 2);
-            paper.DrawLine(pen1, 625, 325, e.X, e.Y);
+            if (e.Button == MouseButtons.Left)
+            {
+                Graphics paper = pictureBoxDisplay.CreateGraphics();
+                Pen pen1 = new Pen(colorDialog1.Color, 2);
+                SolidBrush br = new SolidBrush(Color.Orange);
+                //paper.DrawLine(pen1, 625, 325, e.X, e.Y);
+                //paper.DrawLine(pen1, rand.Next(pictureBoxDisplay.Width + 1), rand.Next(pictureBoxDisplay.Height + 1), e.X, e.Y);
+                paper.FillEllipse(br, e.X, e.Y, 50, 50);
+                paper.DrawEllipse(pen1, e.X, e.Y, 50, 50);
+            }
         }
 
         private void pictureBoxDisplay_Click(object sender, EventArgs e)
