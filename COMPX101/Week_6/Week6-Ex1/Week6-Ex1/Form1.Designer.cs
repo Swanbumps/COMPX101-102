5d00ea5d77be0ce7c188ccfcf74e6d820c75d1c7
Swanbumps <41099838+Swanbumps@users.noreply.github.com>
2020-09-13 23:18:16 +1200

    ex1 work

---
+++ COMPX101/Week_6/Week6-Ex1/Week6-Ex1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Week6_Ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDrawPhone = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDrawPhone
+            // 
+            this.buttonDrawPhone.Location = new System.Drawing.Point(12, 11);
+            this.buttonDrawPhone.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonDrawPhone.Name = "buttonDrawPhone";
+            this.buttonDrawPhone.Size = new System.Drawing.Size(112, 30);
+            this.buttonDrawPhone.TabIndex = 3;
+            this.buttonDrawPhone.Text = "Draw Phone";
+            this.buttonDrawPhone.UseVisualStyleBackColor = true;
+            this.buttonDrawPhone.Click += new System.EventHandler(this.buttonDrawPhone_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(375, 42);
+            this.pictureBoxDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(400, 730);
+            this.pictureBoxDisplay.TabIndex = 2;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Draw Balls";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 139);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 799);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDrawPhone);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDrawPhone;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ COMPX101/Week_6/Week6-Ex1/Week6-Ex1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6_Ex1
+{
+    public partial class Form1 : Form
+    {
+        //Name:Alec Bryant
+        //ID  :1534218
+
+        //size of a lotto ball
+        const int BALL_SIZE = 40;
+        //size of the gap between lotto balls
+        const int GAP_SIZE = 10;
+        // the coordinates of the top, left corner of the display of the phone
+        const int DISPLAY_LEFT = 40;
+        const int DISPLAY_TOP = 140;
+        //the width and height of the display area of the phone
+        const int DISPLAY_WIDTH = 320;
+        const int DISPLAY_HEIGHT = 460;
+
+        //NOTE: If the display looks wrong in your screen resolution, 
+        //please check that the picture box is 400 x 730 pixels in size
+        //and adjust the form to be slightly larger than that.
+        //Stupid Visual Studio changes the form size based on your screen resolution - gah!
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDrawPhone_Click(object sender, EventArgs e)
+        {
+            //set the background image of the picture box to display the phone
+            pictureBoxDisplay.BackgroundImage = Properties.Resources.iPhone;
+        }
+    }
+}
