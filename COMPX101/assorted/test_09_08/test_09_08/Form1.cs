9b62168ed3d4e9d73f7a6d7816bc1220a3f66b62
Swanbumps <41099838+Swanbumps@users.noreply.github.com>
2020-09-13 22:33:50 +1200

    fak

---
+++ COMPX101/assorted/test_09_08/test_09_08/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_09_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int zzzz = 0;
+            for (int i = 0; i < 10; i += 2)
+            {
+                for (int j = 0; j < 10; j += 3)
+                {
+                    zzzz++;
+                }
+            }
+            MessageBox.Show(zzzz.ToString()+" ");
+            int x = 100;
+            do
+            {
+                x = x + 2;
+            } while (x % 2 == 0);
+            Console.WriteLine("" + x);
+        }
+    }
+}
