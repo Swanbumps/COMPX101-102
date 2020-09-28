394cb970b5b929f8811ba1b4a47cd4e27fd0be56
Swanbumps <41099838+Swanbumps@users.noreply.github.com>
2020-09-28 15:34:17 +1300

    week complete

---
+++ COMPX101/Week_8/Week_8_ex2/Week_8_ex2/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Week_8_ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonProcessFile = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonProcessFile
+            // 
+            this.buttonProcessFile.Location = new System.Drawing.Point(13, 13);
+            this.buttonProcessFile.Name = "buttonProcessFile";
+            this.buttonProcessFile.Size = new System.Drawing.Size(95, 23);
+            this.buttonProcessFile.TabIndex = 0;
+            this.buttonProcessFile.Text = "Process File";
+            this.buttonProcessFile.UseVisualStyleBackColor = true;
+            this.buttonProcessFile.Click += new System.EventHandler(this.buttonProcessFile_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(13, 43);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(95, 23);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 77);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonProcessFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonProcessFile;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ COMPX101/Week_8/Week_8_ex2/Week_8_ex2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_8_ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double CalculateDPS(int Weapon_Damage, double Weapon_Speed)
+        {
+            double Weapon_DPS = (double)Weapon_Damage * Weapon_Speed;
+            return Weapon_DPS;
+        }
+
+        private void buttonProcessFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //define variables
+                StreamReader reader;
+                StreamWriter writer;
+                string Weapon_Name;
+                string Weapon_Type;
+                int Weapon_Damage;
+                double Weapon_Speed;
+                double Weapon_DPS;
+                int line;
+
+                //define dialog filter
+                openFileDialog1.Filter = "Text files (*.txt)|*.txt";
+                //open file selection dialog
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            //line count for error
+                            line = 0;
+                            //open file
+                            reader = File.OpenText(openFileDialog1.FileName);
+                            writer = File.CreateText(saveFileDialog1.FileName);
+
+                            writer.WriteLine("Weapon Name".PadRight(25) + "Weapon Type".PadRight(15) + "Damage".PadRight(15) + "Attack Speed".PadRight(15) + "DPS".PadRight(8));
+
+                            //run through all lines
+                        while (!reader.EndOfStream)
+                            {
+                                //read next line
+                                Weapon_Name = reader.ReadLine();
+                                Weapon_Type = reader.ReadLine();
+                                Weapon_Damage = int.Parse(reader.ReadLine());
+                                Weapon_Speed = double.Parse(reader.ReadLine());
+                                Weapon_DPS = CalculateDPS(Weapon_Damage, Weapon_Speed);
+                                line++;
+                                writer.WriteLine(Weapon_Name.PadRight(25) + Weapon_Type.PadRight(15) + Weapon_Damage.ToString().PadRight(15) + Weapon_Speed.ToString().PadRight(15) + Weapon_DPS.ToString().PadRight(10));
+                            }
+                            writer.WriteLine("weapon count: " + line);
+                            reader.Close();
+                            writer.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+
+    
+}
