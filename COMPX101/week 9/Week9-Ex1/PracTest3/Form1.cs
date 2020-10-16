a61ee77da6d5da7c95aa2008d3675f4235773839
Swanbumps <41099838+Swanbumps@users.noreply.github.com>
2020-10-16 15:41:48 +1300

    :(

---
+++ COMPX101/week 9/Week9-Ex1/PracTest3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PracTest3
+{
+    public partial class Form1 : Form
+    {
+        //Name:
+        //ID  :
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //The width of a bar in the bar graph
+        const int BAR_WIDTH = 25;
+
+        //the gap between bars in the bar graph
+        const int GAP = 5;
+
+        //the factor to scale the the graph by to make it fit nicely in the the picturebox
+        const int SCALE_FACTOR = 15;
+
+        /// <summary>
+        /// Draws a vertical bar that is part of a bar graph.
+        /// i.e. It fills a rectangle at position (x,y) with the specified colour.
+        /// Then draws a black outline for the rectangle.
+        /// Uses the BAR_WIDTH constant for the size of the rectangle.
+        /// </summary>
+        /// <param name="paper">The Graphics object to draw on.</param>
+        /// <param name="x">The x position of the top left corner of the rectangle.</param>
+        /// <param name="y">The y position of the top left corner of the rectangle.</param>
+        /// <param name="colour">The colour to fill the background of the rectangle with.</param>
+        private void DrawABar(Graphics paper, int x, int y, int length, Color colour)
+        {
+            //create a brush of specified colour and fill background with this colour 
+            SolidBrush brush = new SolidBrush(colour);
+            paper.FillRectangle(brush, x, y,BAR_WIDTH, length);
+
+            //draw outline in black
+            paper.DrawRectangle(Pens.Black, x, y, BAR_WIDTH, length);
+        }
+
+        
+
+    }
+}
