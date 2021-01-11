76cf4133d5e582bff85739ab11ee6158fed09ab2
Swanbumps <Swanbumps@users.noreply.github.com>
2021-01-12 12:54:28 +1300

    thing

---
+++ COMPX102/assignment 2/assignment2_1534218_Alec-Bryant/assignment 2/assignment 2/HotelRoom.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+	
+	class HotelRoom
+    {
+		public enum RoomType { SingleRoom, QueenStudio, KingStudio, TwoBedroomUnit, LuxurySuite }
+		
+		private int _roomNumber;
+		private int _floor;
+		private RoomType _typeOfRoom;
+		private bool _riverView;
+		private decimal _rate;
+		private bool _booked;
+
+		public HotelRoom(int roomNumber,int floor,RoomType typeOfRoom, bool riverView,decimal rate, bool booked)
+        {
+			_roomNumber = roomNumber;
+			_floor = floor;
+			_typeOfRoom = typeOfRoom;
+			_riverView = riverView;
+			_rate = rate;
+			_booked = booked;
+        }
+
+
+
+
+		public int roomNumber
+        {
+            get { return _roomNumber; }
+            set {
+				if (value > 0)
+				{
+					_roomNumber = value;
+				}
+				else
+				{
+					throw new ArgumentOutOfRangeException
+					  ("The value " + value + " is out of range.");
+				}
+			}
+        }
+		public int roomNumber
+		{
+			get { return _roomNumber; }
+			set
+			{
+				if (value > 0)
+				{
+					_roomNumber = value;
+				}
+				else
+				{
+					throw new ArgumentOutOfRangeException
+					  ("The value " + value + " is out of range.");
+				}
+			}
+		}
+	}
+}
