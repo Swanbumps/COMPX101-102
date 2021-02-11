5c0599b452bbe63ac41dff47980b4232006ebd3d
Swanbumps_B50 <Swanbumps_B50@users.noreply.github.com>
2021-02-12 12:54:06 +1300

    Update Sprite.cs

--- COMPX102/Assignment 4/Assignment 4/Car Soccer/Car Soccer/Sprite.cs
+++ COMPX102/Assignment 4/Assignment 4/Car Soccer/Car Soccer/Sprite.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Car_Soccer
 {
     class Sprite
     {
+        double speed;
+        Point location;
+        double angle;
+
+
+
+
     }
 }
